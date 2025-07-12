43d27d6805ba9fab930783479506b884dc8a1dbd
sogi123456 <huynhchiduy113@gmail.com>
2025-07-12 18:07:59 +0700

    sualailogin

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,78 +1,191 @@
 <!DOCTYPE html>
 <html lang="en">
+	<head>
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+		<meta name="description" content="">
+		<meta name="author" content="">
+	    <meta name="keywords" content="MediaCenter, Template, eCommerce">
+	    <meta name="robots" content="all">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	    <title>Flipmart premium HTML5 & CSS3 Template</title>
 
-    <!------ Include the above in your HEAD tag ---------->
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            background-color: #17a2b8;
-            height: 100vh;
-        }
+	    <!-- Bootstrap Core CSS -->
+	    <link rel="stylesheet" href="assets\css\bootstrap.min.css">
+	    
+	    <!-- Customizable CSS -->
+	    <link rel="stylesheet" href="assets\css\main.css">
+	    <link rel="stylesheet" href="assets\css\blue.css">
+	    <link rel="stylesheet" href="assets\css\owl.carousel.css">
+		<link rel="stylesheet" href="assets\css\owl.transitions.css">
+		<link rel="stylesheet" href="assets\css\animate.min.css">
+		<link rel="stylesheet" href="assets\css\rateit.css">
+		<link rel="stylesheet" href="assets\css\bootstrap-select.min.css">
 
-        #login .container #login-row #login-column #login-box {
-            margin-top: 120px;
-            max-width: 600px;
-            height: 320px;
-            border: 1px solid #9C9C9C;
-            background-color: #EAEAEA;
-        }
+		
 
-        #login .container #login-row #login-column #login-box #login-form {
-            padding: 20px;
-        }
+		
+		<!-- Icons/Glyphs -->
+		<link rel="stylesheet" href="assets\css\font-awesome.css">
 
-        #login .container #login-row #login-column #login-box #login-form #register-link {
-            margin-top: -85px;
-        }
-    </style>
-</head>
+        <!-- Fonts --> 
+		<link href='http://fonts.googleapis.com/css?family=Roboto:300,400,500,700' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,600italic,700,700italic,800' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 
-<body>
-    <div id="login">
-        <h3 class="text-center text-white pt-5">Login</h3>
-        <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-6">
-                    <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" action="{{ route('login') }}" method="post">
-                            @csrf
-                       
-                            <h3 class="text-center text-info">Login</h3>
-                            <div class="form-group">
-                                <label for="username" class="text-info">Username:</label><br>
-                                <input type="text" name="email" id="username" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="password" class="text-info">Password:</label><br>
-                                <input type="password" name="password" id="password" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="remember-me" class="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
-                                <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
-                            </div>
-                                 @if($errors->any())
-                            <div class="alert alert-danger">
-                                {{ $errors->first() }}
-                            </div>
-                            @endif
-                            <div id="register-link" class="text-right">
-                                <a href="{{ route('register') }}" class="text-info">Register here</a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+	</head>
+    <body class="cnt-home">
+		<!-- ============================================== HEADER ============================================== -->
+@include('partials.headerKH')
+<!-- ============================================== HEADER : END ============================================== -->
+<div class="breadcrumb">
+	<div class="container">
+		<div class="breadcrumb-inner">
+			<ul class="list-inline list-unstyled">
+				<li><a href="home">Home</a></li>
+				<li class='active'>Login</li>
+			</ul>
+		</div><!-- /.breadcrumb-inner -->
+	</div><!-- /.container -->
+</div><!-- /.breadcrumb -->
+
+<div class="body-content">
+	<div class="container">
+		<div class="sign-in-page">
+			<div class="row">
+				<!-- Hiển thị thông báo lỗi và thành công -->
+				@if ($errors->any())
+				    <div class="col-md-12">
+				        <div class="alert alert-danger">
+				            <ul style="margin: 0;">
+				                @foreach ($errors->all() as $error)
+				                    <li>{{ $error }}</li>
+				                @endforeach
+				            </ul>
+				        </div>
+				    </div>
+				@endif
+
+				@if (session('success'))
+				    <div class="col-md-12">
+				        <div class="alert alert-success">
+				            {{ session('success') }}
+				        </div>
+				    </div>
+				@endif
+
+				<!-- Sign-in -->			
+<div class="col-md-6 col-sm-6 sign-in">
+    <h4 class="">Sign in</h4>
+    <p class="">Hello, Welcome to your account.</p>
+    <div class="social-sign-in outer-top-xs">
+        <a href="#" class="facebook-sign-in"><i class="fa fa-facebook"></i> Sign In with Facebook</a>
+        <a href="#" class="twitter-sign-in"><i class="fa fa-twitter"></i> Sign In with Twitter</a>
     </div>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-</body>
+    <form class="register-form outer-top-xs" role="form" action="{{ route('login') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label class="info-title" for="login_email">Email Address <span>*</span></label>
+            <input type="email" class="form-control unicase-form-control text-input" id="login_email" name="email" value="{{ old('email') }}" required>
+        </div>
+      	<div class="form-group">
+            <label class="info-title" for="login_password">Password <span>*</span></label>
+            <input type="password" class="form-control unicase-form-control text-input" id="login_password" name="password" required>
+        </div>
+        <div class="radio outer-xs">
+          	<label>
+            	<input type="checkbox" name="remember" id="remember" value="1">Remember me!
+          	</label>
+          	<a href="#" class="forgot-password pull-right">Forgot your Password?</a>
+        </div>
+      	<button type="submit" class="btn-upper btn btn-primary checkout-page-button">Login</button>
+    </form>					
+</div>
+<!-- Sign-in -->
 
+<!-- create a new account -->
+<div class="col-md-6 col-sm-6 create-new-account">
+    <h4 class="checkout-subtitle">Create a new account</h4>
+    <p class="text title-tag-line">Create your new account.</p>
+    <form class="register-form outer-top-xs" role="form" action="{{ route('register') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label class="info-title" for="register_email">Email Address <span>*</span></label>
+            <input type="email" class="form-control unicase-form-control text-input" id="register_email" name="email" value="{{ old('email') }}" required>
+        </div>
+        <div class="form-group">
+            <label class="info-title" for="register_name">Name <span>*</span></label>
+            <input type="text" class="form-control unicase-form-control text-input" id="register_name" name="name" value="{{ old('name') }}" required>
+        </div>
+        <div class="form-group">
+            <label class="info-title" for="register_phone">Phone Number <span>*</span></label>
+            <input type="tel" class="form-control unicase-form-control text-input" id="register_phone" name="phone" value="{{ old('phone') }}" required>
+        </div>
+        <div class="form-group">
+            <label class="info-title" for="register_password">Password <span>*</span></label>
+            <input type="password" class="form-control unicase-form-control text-input" id="register_password" name="password" required>
+        </div>
+        <div class="form-group">
+            <label class="info-title" for="password_confirmation">Confirm Password <span>*</span></label>
+            <input type="password" class="form-control unicase-form-control text-input" id="password_confirmation" name="password_confirmation" required>
+        </div>
+        <button type="submit" class="btn-upper btn btn-primary checkout-page-button">Sign Up</button>
+    </form>
+</div>
+<!-- create a new account -->			</div><!-- /.row -->
+		</div><!-- /.sigin-in-->
+</div><!-- /.container -->
+</div><!-- /.body-content -->
+<!-- ============================================================= FOOTER ============================================================= -->
+@include('partials.footerKH')
+<!-- ============================================================= FOOTER : END============================================================= -->
+
+
+	<!-- For demo purposes – can be removed on production -->
+	
+	
+	<!-- For demo purposes – can be removed on production : End -->
+
+	<!-- JavaScripts placed at the end of the document so the pages load faster -->
+	<script src="assets\js\jquery-1.11.1.min.js"></script>
+	
+	<script src="assets\js\bootstrap.min.js"></script>
+	
+	<script src="assets\js\bootstrap-hover-dropdown.min.js"></script>
+	<script src="assets\js\owl.carousel.min.js"></script>
+	
+	<script src="assets\js\echo.min.js"></script>
+	<script src="assets\js\jquery.easing-1.3.min.js"></script>
+	<script src="assets\js\bootstrap-slider.min.js"></script>
+    <script src="assets\js\jquery.rateit.min.js"></script>
+    <script type="text/javascript" src="assets\js\lightbox.min.js"></script>
+    <script src="assets\js\bootstrap-select.min.js"></script>
+    <script src="assets\js\wow.min.js"></script>
+	<script src="assets\js\scripts.js"></script>
+
+	<!-- For demo purposes – can be removed on production -->
+	
+	<script src="switchstylesheet/switchstylesheet.js"></script>
+	
+	<script>
+		$(document).ready(function(){ 
+			$(".changecolor").switchstylesheet( { seperator:"color"} );
+			$('.show-theme-options').click(function(){
+				$(this).parent().toggleClass('open');
+				return false;
+			});
+		});
+
+		$(window).bind("load", function() {
+		   $('.show-theme-options').delay(2000).trigger('click');
+		});
+	</script>
+	<!-- For demo purposes – can be removed on production : End -->
+
+	
+
+</body>
 </html>
